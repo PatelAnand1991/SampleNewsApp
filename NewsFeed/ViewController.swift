10d2b44de6243ae32482ddfc290cb07881d19732
anand <patelanand1991@gmail.com>
2021-07-09 17:52:11 +0530

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Karan Bhatt on 09/07/21.
+//  Copyright © 2021 Anand Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
