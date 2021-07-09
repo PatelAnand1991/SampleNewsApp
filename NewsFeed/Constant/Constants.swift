3865829e700cbce7f638e33ff1ceb528672f324c
anand <patelanand1991@gmail.com>
2021-07-09 18:12:08 +0530

    Created Constants

---
+++ NewsFeed/Constant/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  NewsFeed
+//
+//  Created by Karan Bhatt on 09/07/21.
+//  Copyright © 2021 Anand Patel. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: General Constants
+
+
+let BaseUrl = "https://newsapi.org/v2/"
+let ApiKey = "a13d06a9e79448f19acb9df5a537e934"
+
+
+// MARK: - WEBSERVICE NAME -
+
+enum TAGNAME: String {
+case Topheadlines = "top-headlines"
+}
